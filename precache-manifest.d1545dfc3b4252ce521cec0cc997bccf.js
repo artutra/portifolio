6d41b4e38026a5932c892160cf09c60f069760d8
Artur Miranda <ldm.artur@gmail.com>
2020-09-21 20:01:29 -0300

    Updates

---
+++ precache-manifest.d1545dfc3b4252ce521cec0cc997bccf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88927bbb47475104088f467f6be4b291",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "447149cd465ab1450691",
+    "url": "/portifolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbad55bd498c23830931",
+    "url": "/portifolio/static/js/2.4a0e4a59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portifolio/static/js/2.4a0e4a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447149cd465ab1450691",
+    "url": "/portifolio/static/js/main.e0c44fb6.chunk.js"
+  },
+  {
+    "revision": "9ad50937adffe7b02ae7",
+    "url": "/portifolio/static/js/runtime-main.4d860e73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portifolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
